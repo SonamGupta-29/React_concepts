99f03ca69622266a53849226ec251f92dac70440
Sonam Gupta <2997sonamgeda@gmail.com>
2024-03-13 08:46:49 +0530

    hello program, prop, destructring, component

--- basic_task/src/App.jsx
+++ basic_task/src/App.jsx
@@ -1,5 +1,14 @@
+import Hello from "./component/Hello";
+
 function App() {
-  return <h1>Hello world!!</h1>
+  //  return <h1>Hello world!</h1>
+
+  return (
+    <div className="App">
+      {/* props concepts */}
+      <Hello name="Sonam" message="Hi" emoji="🖐️" /> 
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ basic_task/src/component/Hello.jsx
@@ -0,0 +1,29 @@
+// component
+// function Hello() {
+//     return <h1>Component</h1>;
+// }
+
+// export default Hello;
+
+// props
+// function Hello(props) {
+//     console.log(props)
+//     return(
+//    <div>
+//      <h1>Hello! {props.name} {props.message}</h1>
+//    </div>
+//    )
+// }
+
+// export default Hello;
+
+// destructuring props
+function Hello({name, message, emoji}) {
+    return(
+   <div>
+     <h1>Hello! {name} {message} {emoji}</h1>
+   </div>
+   )
+}
+
+export default Hello;
